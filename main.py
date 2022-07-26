063e939af73a99cde473d10032e234c24186b415
Robloxthot <67937010+Roblox-Thot@users.noreply.github.com>
2022-07-26 19:10:24 -0400

    roblox is held up by a string

--- main.py
+++ main.py
@@ -13,28 +13,33 @@ else:
 
 dataURL = sussion.get("https://thumbnails.roblox.com/v1/users/avatar-3d?userId=" + userID).json()
 if dataURL["state"] == "Blocked":
+    # I think roblox blocks banned users from my tests
     print("Can't get avatar, user is banned?")
     exit()
 
 data = sussion.get(dataURL["imageUrl"]).json()
 
-materialData = sussion.get("https://t0.rbxcdn.com/" + data["mtl"])
+def url(hash):
+    # Returns the url needed from roblox's hash
+    # Thanks to this sexy person: https://devforum.roblox.com/t/a/1274498/2 <3
+    i = 31
+    for char in hash[:32]:
+        i ^= ord(char)  # i ^= int(char, 16) also works
+    return f"https://t{i%8}.rbxcdn.com/{hash}"
 
 with open(f"files/{username}.mtl", "wb") as file:
+    materialData = sussion.get(url(data["mtl"]))
     file.write(materialData.content)
 
 with open(f"files/{username}.obj", "wb") as file:
     # Roblox changes this url from time to time, so we have to check for it
-    for i in range(1,6):
-        objectData = sussion.get(f"https://t{i}.rbxcdn.com/" + data["obj"])
-        if objectData.status_code == 200:
-            matHeader = bytes(f"mtllib {username}.mtl\n\n", 'utf-8')
-            file.write(matHeader+objectData.content)
-            break
+    objectData = sussion.get(url(data["obj"]))
+    matHeader = bytes(f"mtllib {username}.mtl\n\n", 'utf-8')
+    file.write(matHeader+objectData.content)
 
 for list in data["textures"]:
     with open(f"files/{list}", "wb") as file:
-        file.write(sussion.get("https://t2.rbxcdn.com/" + list).content)
+        file.write(sussion.get(url(list)).content)
 
 # TODO: #1 rename textures to match the name of the object
 # for list in range(len(data["textures"])):
