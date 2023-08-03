35a946681b6d507852befa2bf6303a6faa539dd8
Sourcery AI <>
2023-08-03 07:53:02 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def url(hash):
 if input("(U)sername or user(I)D: ").lower() == "u":
     username = input("Username: ")
     try:
-        userID = sussion.get("https://api.roblox.com/users/get-by-username?username=" + username).json()["Id"]
+        userID = sussion.get(
+            f"https://api.roblox.com/users/get-by-username?username={username}"
+        ).json()["Id"]
         userID = str(userID) # Converts userID to string
     except:
         print("Something went wrong. (user doesn't exist/banned)")
@@ -22,12 +24,16 @@ if input("(U)sername or user(I)D: ").lower() == "u":
 else:
     userID = input("User ID: ")
     try:
-        username = sussion.get("https://users.roblox.com/v1/users/" + userID).json()["name"]
+        username = sussion.get(
+            f"https://users.roblox.com/v1/users/{userID}"
+        ).json()["name"]
     except:
         print("Something went wrong. (user doesn't exist/banned)")
         exit()
 
-dataURL = sussion.get("https://thumbnails.roblox.com/v1/users/avatar-3d?userId=" + userID)
+dataURL = sussion.get(
+    f"https://thumbnails.roblox.com/v1/users/avatar-3d?userId={userID}"
+)
 if dataURL.json()["state"] == "Blocked":
     # I think roblox blocks banned users from my tests
     print("Can't get avatar, user is banned?")
@@ -40,7 +46,9 @@ elif dataURL.status_code != 200:
 try:
     data = sussion.get(dataURL.json()["imageUrl"]).json()
 except:
-    dataURL = sussion.get("https://thumbnails.roblox.com/v1/users/avatar-3d?userId=" + userID)
+    dataURL = sussion.get(
+        f"https://thumbnails.roblox.com/v1/users/avatar-3d?userId={userID}"
+    )
     data = sussion.get(dataURL.json()["imageUrl"]).json()
 
 # Download the object file
