1f41305e636975e12c6766a19edb40096b763f1b
Roblox_Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-03-21 15:15:05 -0400

    clean up

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
-import requests
+from requests import session
 
-sussion = requests.session()
+session = session()
 
-def url(hash):
-    # Returns the url needed from roblox's hash
-    # Thanks to this sexy person: https://devforum.roblox.com/t/a/1274498/2 <3
+def url(hash) -> str:
+    """
+    Returns the url needed from Roblox's hash.
+
+    Args:
+        hash (str): The hash from Roblox.
+
+    Returns:
+        str: The generated URL based on the hash.
+    """
     i = 31
     for char in hash[:32]:
-        i ^= ord(char)  # i ^= int(char, 16) also works
+        i ^= ord(char)
     return f"https://t{i%8}.rbxcdn.com/{hash}"
 
-# ask if the user wants to give username or userID
+# Ask if the user wants to give username or userID
 if input("(U)sername or user(I)D: ").lower() == "u":
     username = input("Username: ")
     try:
-        userID = sussion.get(
+        userID = session.get(
             f"https://api.roblox.com/users/get-by-username?username={username}"
         ).json()["Id"]
         userID = str(userID) # Converts userID to string
-    except:
+    except Exception:
         print("Something went wrong. (user doesn't exist/banned)")
         exit()
 else:
     userID = input("User ID: ")
     try:
-        username = sussion.get(
+        username = session.get(
             f"https://users.roblox.com/v1/users/{userID}"
         ).json()["name"]
-    except:
+    except Exception:
         print("Something went wrong. (user doesn't exist/banned)")
         exit()
 
-dataURL = sussion.get(
+dataURL = session.get(
     f"https://thumbnails.roblox.com/v1/users/avatar-3d?userId={userID}"
 )
 if dataURL.json()["state"] == "Blocked":
@@ -44,25 +51,25 @@ elif dataURL.status_code != 200:
 
 # Grab the data needed to download files later
 try:
-    data = sussion.get(dataURL.json()["imageUrl"]).json()
-except:
-    dataURL = sussion.get(
+    data = session.get(dataURL.json()["imageUrl"]).json()
+except Exception:
+    dataURL = session.get(
         f"https://thumbnails.roblox.com/v1/users/avatar-3d?userId={userID}"
     )
-    data = sussion.get(dataURL.json()["imageUrl"]).json()
+    data = session.get(dataURL.json()["imageUrl"]).json()
 
 # Download the object file
 with open(f"files/{username}.obj", "wb") as file:
     print("Downloading object file...                ", end="\r")
     # Roblox changes this url from time to time, so we have to check for it
-    objectData = sussion.get(url(data["obj"]))
+    objectData = session.get(url(data["obj"]))
     matHeader = bytes(f"mtllib {username}.mtl\n\n", 'utf-8')
     file.write(matHeader+objectData.content)
 
 # Download the material file
 with open(f"files/{username}.mtl", "w") as file:
     print("Downloading material file...              ", end="\r")
-    materialData = sussion.get(url(data["mtl"])).text
+    materialData = session.get(url(data["mtl"])).text
     for list in range(len(data["textures"])):
         materialData = materialData.replace(data["textures"][list], f"{username}_{str(list)}.png")
     file.write(materialData)
@@ -71,6 +78,6 @@ with open(f"files/{username}.mtl", "w") as file:
 for list in range(len(data["textures"])):
     print("Downloading textures...                   ",end="\r")
     with open(f"files/{username}_{str(list)}.png", "wb") as file:
-        file.write(sussion.get(url(data["textures"][list])).content)
+        file.write(session.get(url(data["textures"][list])).content)
 
 print("Done, files are in files folder.")
